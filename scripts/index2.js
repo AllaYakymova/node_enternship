2f88ed4602216d065b9cb37de38a651706ee0fa3
Allegra <allochkaja112@gmail.com>
2021-03-19 17:50:52 +0200

    HW1_task2 #2

--- scripts/index2.js
+++ scripts/index2.js
@@ -1,12 +1,14 @@
 'use strict';
 
-let x = '4of Fo1r pe6ople g11ood th5e the2 sfp0';
+let x = '4of Fo1r pe6ople g141ood th5e the2 sfp0';
 
 function arrangeWordsByNumbers(string) {
+    let getNumber = (word) => +word.match(/\d+/)[0];
+
     let newString = string.split(' ')
         .map(item => {
-            let z = +item.match(/\d+/)[0];
-            if (0 < z && z < 10) {
+            let number = getNumber(item);
+            if (0 < number && number < 10) {
                 return item
             } else {
                 return false
@@ -14,8 +16,8 @@ function arrangeWordsByNumbers(string) {
         })
         .filter(item => item)
         .sort((a, b) => {
-            let numberA = a.match(/\d/)[0];
-            let numberB = b.match(/\d/)[0];
+            let numberA = getNumber(a);
+            let numberB = getNumber(b);
             return numberA > numberB ? 1 : -1;
         })
         .join(" ");
