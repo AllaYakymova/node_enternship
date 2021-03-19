c1a16699d4a5d5a7093739a9220f7fb9606c467f
Allegra <allochkaja112@gmail.com>
2021-03-19 19:00:23 +0200

    HW1_task1 #1

--- scripts/index1.js
+++ scripts/index1.js
@@ -1,2 +1,18 @@
 'use strict';
 
+// один вариант преобразования натурального числа в бинарное
+let integerToBinary = integer => (integer >>> 0).toString(2);
+
+function integerToBinarySum(integer) {
+    if (Number.isInteger(integer) && integer > 0) {
+        let binary = integerToBinary(integer);
+        return binary
+            .split('')
+            .filter(num => +num === 1).length;
+    } else {
+        console.log('Enter non-negative integer')
+    }
+}
+
+integerToBinarySum(1234);
+
