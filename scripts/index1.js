feb7b50cf0178c6b164f4fac680f238465e56d56
Allegra <allochkaja112@gmail.com>
2021-03-20 07:43:39 +0200

    HW1_task1 #2 approach

--- scripts/index1.js
+++ scripts/index1.js
@@ -1,18 +1,35 @@
 'use strict';
 
-// один вариант преобразования натурального числа в бинарное
-let integerToBinary = integer => (integer >>> 0).toString(2);
-
 function integerToBinarySum(integer) {
     if (Number.isInteger(integer) && integer > 0) {
         let binary = integerToBinary(integer);
         return binary
             .split('')
-            .filter(num => +num === 1).length;
+            .reduce((a, b) => +a + +b);
     } else {
-        console.log('Enter non-negative integer')
+        console.log('Enter a non-negative integer')
     }
 }
 
-integerToBinarySum(1234);
+console.log(integerToBinarySum(1234));
 
+
+// один вариант преобразования натурального числа в бинарное
+function integerToBinary (integer) {
+    return (integer >>> 0).toString(2);
+}
+
+// второй вариант преобразования натурального числа в бинарное
+function integerToBinary1(integer) {
+    let rest = integer;
+    let arr = [];
+    while (rest !== 0) {
+        let remainder = rest % 2;
+        rest = Math.floor(rest / 2);
+        arr = [...arr, remainder];
+    }
+    return arr.reverse().join('');
+}
+
+console.log(integerToBinary(1234));
+console.log(integerToBinary1(1234));
