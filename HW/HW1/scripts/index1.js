fedac34a11fd19ead16c72825f5aa8f9bedfa2fd
Allegra <allochkaja112@gmail.com>
2021-03-20 09:35:16 +0200

    Configured eslint. Fixed code

--- HW/HW1/scripts/index1.js
+++ HW/HW1/scripts/index1.js
@@ -1,35 +1,38 @@
-'use strict';
+"use strict";
+
+// #1 option to convert an integer to a binary
+function integerToBinary (integer) {
+  return (integer >>> 0).toString(2);
+}
+
+// #2 option to convert an integer to a binary
+function integerToBinary1(integer) {
+  let rest = integer;
+  let arr = [];
+  while (rest !== 0) {
+    let remainder = rest % 2;
+    rest = Math.floor(rest / 2);
+    arr = [...arr, remainder];
+  }
+  return arr.reverse().join("");
+}
+
 
 function integerToBinarySum(integer) {
-    if (Number.isInteger(integer) && integer > 0) {
-        let binary = integerToBinary(integer);
-        return binary
-            .split('')
-            .reduce((a, b) => +a + +b);
-    } else {
-        console.log('Enter a non-negative integer')
-    }
+  if (Number.isInteger(integer) && integer > 0) {
+    let binary = integerToBinary(integer);
+    return binary
+      .split("")
+      .reduce((a, b) => +a + +b);
+  } else {
+    console.log("Enter a non-negative integer");
+  }
 }
 
 console.log(integerToBinarySum(1234));
 
 
-// один вариант преобразования натурального числа в бинарное
-function integerToBinary (integer) {
-    return (integer >>> 0).toString(2);
-}
 
-// второй вариант преобразования натурального числа в бинарное
-function integerToBinary1(integer) {
-    let rest = integer;
-    let arr = [];
-    while (rest !== 0) {
-        let remainder = rest % 2;
-        rest = Math.floor(rest / 2);
-        arr = [...arr, remainder];
-    }
-    return arr.reverse().join('');
-}
 
 console.log(integerToBinary(1234));
 console.log(integerToBinary1(1234));
--- HW/HW1/scripts/index2.js
+++ HW/HW1/scripts/index2.js
@@ -1,29 +1,28 @@
-'use strict';
+"use strict";
 
-let x = '4of Fo1r pe6ople g14ood th5e the2 sfp0';
-let y ='';
+let x = "4of Fo1r pe6ople g14ood th5e the2 sfp0";
+let y ="";
 
 function arrangeWordsByNumbers(string) {
-    let getNumber = (word) => +word.match(/\d+/)[0];
+  let getNumber = word => +word.match(/\d+/)[0];
 
-    if(string.length !== 0) {
-       return string.split(' ')
-            .map(item => {
-                let number = getNumber(item);
-                if (0 < number && number < 10) {
-                    return item
-                } else {
-                    return false
-                }
-            })
-            .filter(item => item)
-            .sort((a, b) => getNumber(a) > getNumber(b) ? 1 : -1)
-            .join(" ");
-    } else {
-        return string;
-    }
-
+  if(string.length !== 0) {
+    return string.split(" ")
+      .map(item => {
+        let number = getNumber(item);
+        if (0 < number && number < 10) {
+          return item;
+        } else {
+          return false;
+        }
+      })
+      .filter(item => item)
+      .sort((a, b) => (getNumber(a) > getNumber(b) ? 1 : -1))
+      .join(" ");
+  } else {
+    return string;
+  }
 }
 
 console.log(arrangeWordsByNumbers(x));
-console.log('empty string:', arrangeWordsByNumbers(y));
+console.log("empty string:", arrangeWordsByNumbers(y));
