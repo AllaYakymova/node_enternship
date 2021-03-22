b01eacf518f9303ae807d80effa8b2725fb5bc60
Allegra <allochkaja112@gmail.com>
2021-03-22 16:57:26 +0200

    HW_2

--- HW/HW2/index.js
+++ HW/HW2/index.js
@@ -1,44 +1,45 @@
 "use strict";
 //concat
-const concat = (a, b) => [...a, ...b];
+const concat = (a, b) => [...a, ...b].join("");
 
-console.log(concat([1,2,3,4], [5,6,7,8]));
+console.log(concat('qwerty', "ytrewq"));
 
 
 // lastIndexOf
-function lastIndexOf(arr, element) {
+function lastIndexOf(str, element) {
   let indexes = [];
-  arr.forEach((el, index) => el === element && indexes.unshift(index));
+  str.split("").forEach((el, index) => el === element && indexes.unshift(index + 1));
   return indexes[0];
 }
-console.log(lastIndexOf([6,1,2,6,3,4,5,6], 6));
+
+console.log(lastIndexOf("qwerty", "w"));
 
 
 //includes
-const includes = (arr, element) => arr.filter(el => el === element).length > 0;
+const includes = (str, substr) => !!str.match(substr);
 
-console.log(includes([1,2,4,6,8], 5));
+console.log(includes("qwerty", 'er'));
 
 
 // repeat
 const repeat = (str, count) => {
   let newString = "";
-  for(let i = 0; i <= count; i++) {
+  for (let i = 0; i <= count; i++) {
     newString = newString + str;
   }
   return newString;
 };
 
-console.log(repeat('qwerty', 3));
+console.log(repeat("qwerty", 3));
 
 
 // substr
 const substr = (str, start, length) => str.split("").splice(start, length).join("");
 
-console.log(substr('qwerty', 2, 4));
+console.log(substr("qwerty", 2, 4));
 
 
 // substring
 const substring = (str, start, end) => str.split("").slice(start, end).join("");
 
-console.log(substring('qwerty', 2, 6));
+console.log(substring("qwerty", 2, 6));
