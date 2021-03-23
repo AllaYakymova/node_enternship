0c832f9f2087ab92baab64f4db2c4beff2b4f607
Allegra <allochkaja112@gmail.com>
2021-03-23 09:15:17 +0200

    HW_2 fixed lastIndexOf

--- HW/HW2/index.js
+++ HW/HW2/index.js
@@ -2,27 +2,38 @@
 //concat
 const concat = (a, b) => [...a, ...b].join("");
 
-console.log(concat('qwerty', "ytrewq"));
+// console.log(concat('qwerty', "ytrewq"));
 
 
 // lastIndexOf
-const lastIndexOf = (str, substr) => {
+const lastIndexOf = (str, substr, fromIndex ) => {
   let indexes = [];
-  str.split("").forEach((el, index) => el === substr.slice(0,1) && indexes.unshift(index));
-  for(let i = 0; i <= indexes.length; i++) {
-    if(str.substr(indexes[i], substr.length) === substr) {
-      return indexes[i];
+  let lastIndex = 0;
+  substr = substr.toString();
+  str.slice(fromIndex).split("").forEach((el, index) => el === substr.slice(0,1) && indexes.unshift(index));
+  for (let i = 0; i <= indexes.length-1; i++) {
+      if (str.substr(indexes[i], substr.length) === substr) {
+        return indexes[i];
+      }
+      lastIndex = -1;
     }
+  if (lastIndex === -1 || indexes.length === 0) {
+    return -1;
   }
 };
 
-console.log(lastIndexOf("qwertyererrh", "er"));
+console.log(lastIndexOf("С5ини,5й кит, каси,5тка", 5));
+console.log(lastIndexOf("канал", "а", 2));
+console.log(lastIndexOf("канал", "а", 0));
+console.log(lastIndexOf("канал", "а,"));
+
+
 
 
 //includes
 const includes = (str, substr) => !!str.match(substr);
 
-console.log(includes("qwerty", 'er'));
+// console.log(includes("qwerty", 'er'));
 
 
 // repeat
@@ -34,16 +45,16 @@ const repeat = (str, count) => {
   return newString;
 };
 
-console.log(repeat("qwerty", 3));
+// console.log(repeat("qwerty", 3));
 
 
 // substr
 const substr = (str, start, length) => str.split("").splice(start, length).join("");
 
-console.log(substr("qwerty", 2, 4));
+// console.log(substr("qwerty", 2, 4));
 
 
 // substring
 const substring = (str, start, end) => str.split("").slice(start, end).join("");
 
-console.log(substring("qwerty", 2, 6));
+// console.log(substring("qwerty", 2, 6));
