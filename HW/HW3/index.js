358225e2172d25397b848a314a8505ca71836981
Allegra <allochkaja112@gmail.com>
2021-03-24 10:35:46 +0200

    HW_3

--- HW/HW3/index.js
+++ HW/HW3/index.js
@@ -1,6 +1,6 @@
 "use strict";
 let arrayPush = {
-  arr: ['пастернак', 'картошка'],
+  arr: ["пастернак", "картошка"],
 };
 let arrayPop = {
   arr: [1, 2, 3, 4],
@@ -11,75 +11,85 @@ let arrayShift = {
 let arrayUnshift = {
   arr: [1, 2, 3, 4],
 };
-let arrayConcat = {
-  arr: ['пастернак', 'картошка'],
-};
 
 // push добавляет один или более элементов в конец массива и возвращает новую длину массива.
 function push(...arg) {
-  this.arr = [...this.arr, ...arg];
+  const addedArg = [...arg];
+  this.arr = [...this.arr, ...addedArg];
   return this.arr.length;
 }
+
 let pushMutable = push.bind(arrayPush);
 
-console.log('push', pushMutable(1, [2, 3]), arrayPush.arr);
+console.log("push", pushMutable(1, [2, 3]), arrayPush.arr);
 
 // pop удаляет последний элемент из массива и возвращает его значение.
 function pop() {
-  let deletedEl = this.arr[this.arr.length - 1];
-  this.arr = this.arr.filter((el, i) => i < this.arr.length - 1 && el);
+  const deletedEl = this.arr[this.arr.length - 1];
+  this.arr.length = this.arr.length - 1;
   return deletedEl;
 }
+
 let popMutable = pop.bind(arrayPop);
 
-console.log('pop', popMutable(), arrayPop.arr);
+console.log("pop", popMutable(), arrayPop.arr);
 
 // shift удаляет первый элемент из массива и возвращает его значение. Этот метод изменяет длину массива.
 function shift() {
-  let deletedEl = this.arr[0];
-  this.arr[0] = false;
-  this.arr = this.arr.filter(el => el );
+  const deletedEl = this.arr[0];
+  const arrLength = this.arr.length - 1;
+  let resultArr = [];
+  for (let i = 1; i <= arrLength; i++) {
+    resultArr = [...resultArr, this.arr[i]];
+  }
+  this.arr = resultArr;
   return deletedEl;
 }
+
 let shiftMutable = shift.bind(arrayShift);
-console.log('shift', shiftMutable(), arrayShift.arr);
+console.log("shift", shiftMutable(), arrayShift.arr);
 
 // unshift добавляет один или более элементов в начало массива и возвращает новую длину массива.
-function unshift (...arg) {
+function unshift(...arg) {
   this.arr = [...arg, ...this.arr];
   return this.arr.length;
 }
-let unshiftMutable = unshift.bind(arrayUnshift);
-console.log('unshift', unshiftMutable(9,8,7,6,'f'), arrayUnshift.arr);
+
+const unshiftMutable = unshift.bind(arrayUnshift);
+console.log("unshift", unshiftMutable(9, 8, 7, 6, "f"), arrayUnshift.arr);
+
 
 // concat возвращает новый массив, состоящий из массива, на котором он был вызван, соединённого с другими массивами и/или значениями, переданными в качестве аргументов.
-function concat (...arg) {
-  let argArr = arg.reduce((acc, el) => Array.isArray(el) ? [...acc, ...el] : [...acc, el], []);
-  return [...this.arr, ...argArr];
-}
-let concatMutable = concat.bind(arrayConcat);
-console.log('concat', concatMutable(1, [2, 3], {"test": 6}), arrayConcat.arr);
+const concat = (arr, ...arg) => {
+  const [addedArg] = arg;
+  return [...arr, ...addedArg];
+};
+
+const arr = ["пастернак", "картошка"];
+const arrAdded = [1, [2, 3], {"test": 6}];
+console.log("concat", concat(arr, arrAdded), arr);
 
 
+//////////////////////////////////////////////////
 
-let reduceArr = ['Apple', 'Banana', 'Pineapple'];
-let reduceArr2 = ['Apple', 'Banana', 'Pineapple'];
-let reduceArr3 = ['Apple', 'Banana', 'Pineapple'];
+let reduceArr = ["Apple", "Banana", "Pineapple"];
+let reduceArr2 = ["Apple", "Banana", "Pineapple"];
+let reduceArr3 = ["Apple", "Banana", "Pineapple"];
 
 //// REDUCE - MAP
-let reduceMap = reduceArr.reduce((acc, el) => [...acc, el.slice(0,1)], []);
+const reduceMap = arr => arr.reduce((acc, el) => [...acc, el.slice(0, 1)], []);
 
-console.log(reduceMap);
+console.log(reduceMap(reduceArr));
 
 
 //// REDUCE - FILTER
-let reduceFilter = reduceArr2.reduce((acc, el) =>
-  el.slice(0,1).toLowerCase() === 'a' ? [...acc, el] : [...acc], []);
+const reduceFilter = arr => arr.reduce((acc, el) =>
+  el.slice(0, 1).toLowerCase() === "a" ? [...acc, el] : [...acc], []);
 
-console.log(reduceFilter);
+console.log(reduceFilter(reduceArr2));
 
 
 //// REDUCE - FOREACH
-let reduceForEach = reduceArr3.reduce((acc, el, i) => [...acc, `${i+1}: ${el}`], []);
+const reduceForEach = arr => arr.reduce((acc, el, i) => [...acc, `${i + 1}: ${el}`], []);
 
-console.log(reduceForEach);
+console.log(reduceForEach(reduceArr3));
