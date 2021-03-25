3a277bef51870530fc465670f091fdec10810662
Allegra <allochkaja112@gmail.com>
2021-03-25 20:43:12 +0200

    HW5

--- HW/HW5/index.js
+++ HW/HW5/index.js
@@ -5,11 +5,12 @@ function ipsBetween(...arg) {
 
   const validation = (aps) => aps.filter(el => el.split('.').length - 1 === 3 && el.slice(0, 3) <= 239 && el).length === aps.length;
 
-  const iPsCount = (api) => api.split('.').reverse().map((el, i) => +el << (i * 8)).reduce((acc, el) => acc + el);
+  const iPsCount = (api) => api.split('.').reverse().reduce((acc, el, i) => acc + el * Math.pow(2, i * 8), 0);
 
   return validation(aps) && aps[1] > aps[0] ? iPsCount(aps[1]) - iPsCount(aps[0]) : 'Enter correct aps';
 }
 
-console.log(ipsBetween("0.0.0.0", "240.0.0.50"));
-console.log(ipsBetween("20.0.0.10", "20.0.1.0"));
-console.log(ipsBetween("0.1.1.1", "40.4.4.4"));
+
+console.log(ipsBetween("0.0.0.0", "240.255.255.254"));
+console.log(ipsBetween("10.0.0.0", "10.0.1.0"));
+console.log(ipsBetween("0.0.0.1", "40.4.4.4"));
