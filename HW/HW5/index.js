aa07e12e7e1b1651fb7e28f8c0d5eb0cdfcaa911
Allegra <allochkaja112@gmail.com>
2021-03-26 11:14:28 +0200

    HW5 +reg fix

--- HW/HW5/index.js
+++ HW/HW5/index.js
@@ -2,11 +2,11 @@
 
 function ipsBetween(...arg) {
   let [...aps] = arg;
-  const validation = (aps) => aps.filter(el => /^\d{1,4}\.\d{1,3}\.\d{1,3}\.\d{1,3}$/.test(el) && el.slice(0, 3) <= 239 && el).length === aps.length;
+  const validation = (aps) => aps.filter(el => /^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$/.test(el) && el.slice(0, 3) <= 239 && el).length === aps.length;
   const iPsCount = (api) => api.split('.').reverse().reduce((acc, el, i) => acc + el * Math.pow(2, i * 8), 0);
   return validation(aps) && aps[1] > aps[0] ? iPsCount(aps[1]) - iPsCount(aps[0]) : 'Enter correct aps';
 }
 
 console.log(ipsBetween("0.0.0.0", "60.25n.255.254"));
-console.log(ipsBetween("10.0.0.0", "1.0.0.10"));
+console.log(ipsBetween("10.0.0.0", "141.0.0.10"));
 console.log(ipsBetween("0.0.0.1", "40.4.4.4"));
