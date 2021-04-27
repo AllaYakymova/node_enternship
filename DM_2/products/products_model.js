5deab6fa8e2cb8c6a935b5e422847e142a47be06
Allegra <allochkaja112@gmail.com>
2021-04-27 08:13:00 +0300

    demo#2 class ProductsModel

--- DM_2/products/products_model.js
+++ DM_2/products/products_model.js
@@ -1,37 +1,63 @@
+const {client} = require('../config');
 
-exports.productsModel = function (req) {
-  if (req.params.productId) {
-    return `SELECT products.id, product_name, manufactures.manufacture, categories.category, ingridients, units.unit, price, img_link FROM products, categories, manufactures, units WHERE products.id = $1 AND products.id_manufacture = manufactures.id AND products.id_category = categories.id AND products.id_units = units.id;`;
+module.exports = class ProductsModel {
+  constructor(req) {
+    this.req = req;
   }
 
-  if (req.query) {
-    const {manufactures, categories, products} = req.query;
-    const reg = /^([1-9,]*)$/;
-    let arg = [];
-    const scheme = {
-      'manufactures.manufacture': !!manufactures && manufactures,
-      'categories.id': !!categories && reg.test(categories) ? categories.split(',')
-        .filter(el => el) : false,
-      'product_name': !!products && products,
-    };
+  async getAllProducts() {
+    try {
+      const query = `SELECT products.id, product_name, manufactures.manufacture, categories.category FROM products, categories, manufactures WHERE products.id_manufacture = manufactures.id AND products.id_category = categories.id ORDER BY product_name ASC;`;
+      const result = await client.query(query);
+      return result.rows;
+    } catch (e) {
+      console.log(e);
+    }
+  }
 
-    if (Object.values(scheme)
-      .filter(el => el).length !== 0) {
-      for (let query in scheme) {
-        if (scheme[query]) {
-          if (query === 'categories.id') {
-            arg.push(`${query} IN (${scheme[query]})`);
-          } else {
-            arg.push(`${query} ILIKE '${scheme[query]}%'`);
+  async getProductById() {
+    try {
+      const {productId} = this.req.params;
+      const query = `SELECT products.id, product_name, manufactures.manufacture, categories.category, ingridients, units.unit, price, img_link FROM products, categories, manufactures, units WHERE products.id = $1 AND products.id_manufacture = manufactures.id AND products.id_category = categories.id AND products.id_units = units.id;`;
+
+      const result = await client.query(query, [productId]);
+      return result.rows;
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  async searchProducts() {
+    try {
+      const {manufactures, categories, products} = this.req.query;
+      const reg = /^([1-9,]*)$/;
+      let arg = [];
+      const scheme = {
+        'manufactures.manufacture': !!manufactures && manufactures,
+        'categories.id': !!categories && reg.test(categories) ? categories.split(',')
+          .filter(el => el) : false,
+        'product_name': !!products && products,
+      };
+
+      if (Object.values(scheme)
+        .filter(el => el).length !== 0) {
+        for (let query in scheme) {
+          if (scheme[query]) {
+            if (query === 'categories.id') {
+              arg.push(`${query} IN (${scheme[query]})`);
+            } else {
+              arg.push(`${query} ILIKE '${scheme[query]}%'`);
+            }
           }
         }
       }
+      const condition = arg.length !== 0 ? `AND ${arg.join(' AND ')}` : '';
+      const query = `SELECT products.id, product_name, manufactures.manufacture, categories.category, units.unit, price, img_link FROM products, categories, manufactures, units WHERE products.id_manufacture = manufactures.id AND products.id_category = categories.id AND products.id_units = units.id ${condition};`; // проверить внести аругментом
+      const result = await client.query(query);
+      return result.rows;
+    } catch (e) {
+      console.log(e);
     }
-    const condition = arg.length !== 0 ? `AND ${arg.join(' AND ')}` : '';
-    return `SELECT products.id, product_name, manufactures.manufacture, categories.category, units.unit, price, img_link FROM products, categories, manufactures, units WHERE products.id_manufacture = manufactures.id AND products.id_category = categories.id AND products.id_units = units.id ${condition};`;
-  }
-  if(req.baseUrl==='/products') {
-    return `SELECT products.id, product_name, manufactures.manufacture, categories.category FROM products, categories, manufactures WHERE products.id_manufacture = manufactures.id AND products.id_category = categories.id ORDER BY product_name ASC;`;
   }
 };
 
